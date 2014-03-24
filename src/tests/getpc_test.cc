d151af55d733de269ebc5afcdf481bf51a0458fd
Raphael Moreira Zinsly <rzinsly@linux.vnet.ibm.com>
2014-03-24 17:10:10 -0300

    Fixed getpc_test for ppc64 LE.
    This patch fixes the problem with the test about get the pc on ppc64
    Little Endian machines.

--- src/tests/getpc_test.cc
+++ src/tests/getpc_test.cc
@@ -107,7 +107,7 @@ int main(int argc, char** argv) {
   //      ElfW(Addr) gp;      /* global pointer */
   //   };
   // We want the code entry point.
-#if defined(__ia64) || defined(__powerpc64__)     // NOTE: ppc64 is UNTESTED
+#if defined(__ia64) || defined(__powerpc64__) && (defined(__CALL_ELF) && (__CALL_ELF == 1))    // NOTE: ppc64 is UNTESTED
   expected = ((char**)expected)[0];         // this is "ip"
 #endif
 
